d343de111faa363af392bd46cfd828d0716fdbec
keller0707 <kellersedillo@hotmail.com>
2022-08-25 22:51:06 +0530

    Added Defines to Program #1

--- CS 471 - Prog Lang Structure/Programming #1/aliasing.c
+++ CS 471 - Prog Lang Structure/Programming #1/aliasing.c
@@ -2,7 +2,7 @@
  * aliasing.c
  * Made By: Keller Sedillo-Garrido
  * Date: August 18
- *
+ * Input 
  ***/
 
 //Libary
@@ -10,20 +10,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Defines
+#define ABYTE      256 // One Byte
+#define BBYTE    65536 // Two Bytes
+#define CBYTE 16777216 // Three Bytes
+
 int main(){
    //Local Vars	
    int A[100];
    char *S;
 
    //Assign int array
-   A[0] = 'lleK'; //lleK 
-   A[1] = 'S re'; 
-   A[2] = 'lide';
-   A[3] = 'G-ol';
-   A[4] = 'irra';
-   A[5] = 'od'  ;
-   A[6] = '0'   ; //End String
-   A[7] = 'TSET';
+   A[0] = 'K' + 'e' * ABYTE + 'l' * BBYTE + 'l' * CBYTE; // Kell 
+   A[1] = 'e' + 'r' * ABYTE + ' ' * BBYTE + 'S' * CBYTE; // er S
+   A[2] = 'e' + 'd' * ABYTE + 'i' * BBYTE + 'l' * CBYTE; // edil
+   A[3] = 'l' + 'o' * ABYTE + '-' * BBYTE + 'G' * CBYTE; // lo-G
+   A[4] = 'a' + 'r' * ABYTE + 'r' * BBYTE + 'i' * CBYTE; // arri
+   A[5] = 'd' + 'o' * ABYTE;                             // do
+   A[6] = '0';                                           // End String
+   A[7] = 'T';
    
    //Point S to A
    S = (char *) A;
