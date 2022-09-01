0e308cd767b9e175fc76e32ec814d5268c77bdc1
keller0707 <kellersedillo@hotmail.com>
2022-09-01 22:23:46 +0530

    Fixed Bug with Data Mining HW0

--- CS 488 - Data Mining/Homework 0/homework0.py
+++ CS 488 - Data Mining/Homework 0/homework0.py
@@ -23,15 +23,15 @@ print("\n5.) Printing the values of the Survived column:\n",df['Survived'])
 print("\n6.) Number of rows with a Survived value of 1:\n",df['Survived'].value_counts()[1])
 
 #Calculate and print the number of rows that do not have missing values in the 6th column
-print("\n7.) Number of rows that is not missing values:\n",df['Age'].notnull().sum())
+print("\n7.) Number of rows that is not missing values in the Age Column:\n",df['Age'].notnull().sum())
 
 #Calculate the average value of the 6th column
 agedf = df['Age'].dropna()
 print("\n8.) Average Value of the Age Column:\n",agedf.sum()/len(agedf))
 
 #Calculate the average value of the 6th column when the 2nd column has value 1.
-survdf = df[df['Survived'] == 1].dropna() #Drop all N/A From the Data Frame
-print("\n9.) Average Value of the Age Column with the Value 1 in the Survived Column\n",survdf['Age'].sum()/len(survdf))
+survdf = df[df['Survived'] == 1]
+print("\n9.) Average Value of the Age Column with the Value 1 in the Survived Column\n",survdf['Age'].dropna().sum()/len(survdf['Age'].dropna()))
 
 #Draw a scatter plot with the data of the 6th column (“Age” column) and the 10th column(“Fare” column).
 newdf = df[['Age', 'Fare']].dropna().copy()
